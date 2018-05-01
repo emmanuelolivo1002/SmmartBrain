d77cca92c7d68d7353b6ef54ac1ff5462e9d14c8
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-04-30 20:09:38 -0500

    Add Navigation and background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
+import 'tachyons';
+import Navigation from './components/Navigation/Navigation';
 
 class App extends Component {
   render() {
-    return (<div className="App"></div>);
+    return (<div className="App">
+      <Navigation/> {/* <Logo/>
+      <ImageLinkForm/>
+      <FaceRecognition/> */
+      }
+    </div>);
   }
 }
 
