7b95695048cb2127737161c4e79f54290a0e44ae
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-04-30 21:19:01 -0500

    Add particles.js implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,91 @@
 import React, {Component} from 'react';
 import './App.css';
+
+// Packages
 import 'tachyons';
+import Particles from 'react-particles-js';
+
+// Components
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
+const ParticleParams = {
+  particles: {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 1500
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  }
+}
+
 class App extends Component {
   render() {
     return (<div className="App">
+      <Particles className='particles' params={ParticleParams}/>
       <Navigation/>
       <Logo/>
       <Rank/>
