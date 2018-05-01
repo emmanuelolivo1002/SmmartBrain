880c4f6248062b48d6c01b3a2d8f65d4ea25a728
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-04-30 21:06:34 -0500

    Add Rank component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import 'tachyons';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
 class App extends Component {
   render() {
     return (<div className="App">
       <Navigation/>
       <Logo/>
-      <ImageLinkForm/>{/*
+      <Rank/>
+      <ImageLinkForm/> {/*
       <FaceRecognition/> */
       }
     </div>);
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+  //Rank
+  <div>
+    <div className='f3 white'>
+      {'Emmanuel, your current rank is:'}
+    </div>
+    <div className='f1 white'>
+      {'#42'}
+    </div>
+  </div>);
+}
+
+export default Rank;
