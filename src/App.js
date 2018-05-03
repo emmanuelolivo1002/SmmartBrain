6449e2b83ae380898313de28bfdbf19bfc818014
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-05-03 14:33:15 -0500

    Restart initial state when changing routes and signin out

--- src/App.js
+++ src/App.js
@@ -90,25 +90,30 @@ const ParticleParams = {
 // Clarifai API configuration
 const app = new Clarifai.App({apiKey: 'a911ba983b324580987163ea2c293bd4'});
 
+
+// Initial state
+const initialState = {
+  input: '',
+  imageURL: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
+
 class App extends Component {
 
   constructor() {
     super();
     // Setup state object
-    this.state = {
-      input: '',
-      imageURL: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
 
@@ -169,8 +174,9 @@ class App extends Component {
           })
             .then(response => response.json())
             .then(count => {
-               this.setState(Object.assign(this.state.user, { entries: count.entries }));
+               this.setState(Object.assign(this.state.user, { entries: count }));
             })
+            .catch(console.log);
 
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
@@ -181,7 +187,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home')  {
       this.setState({isSignedIn: true})
     }
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -33,7 +33,6 @@ class Signin extends React.Component {
         this.props.onRouteChange('home');
       }
     });
-
   }
 
   render() {
